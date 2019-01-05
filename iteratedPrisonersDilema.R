dfba42eec8be7b1a45d138686a497aa54d467a24
DuncanMcKinnon <duncanmckinnon96@gmail.com>
2019-01-05 11:19:10 -0800

    adding full prev input to function for more complex strategies

--- iteratedPrisonersDilema.R
+++ iteratedPrisonersDilema.R
@@ -1,65 +1,86 @@
 
+### Strategies
 
 optimist <- list(
   name = 'optimist',
-  f = function(prev) {
+  f = function(prev, n) {
     return(0)
   }
 )
 
 pessimist <- list(
   name = 'pessimist',
-  f = function(prev) {
+  f = function(prev, n) {
     return(1)
+  }
+)
+
+
+vengeful <- list(
+  name = 'vengful',
+  f = function(prev, n) {
+    return(1 %in% prev)
+  }
+)
+
+forgiving <- list(
+  name = 'forgiving',
+  f = function(prev, n) {
+    return(!(0 %in% prev))
   }
 )
 
 random_ <- list(
   name = 'random',
-  f = function(prev) {
+  f = function(prev, n) {
     return(sample(c(0, 1), 1, replace = T))
   }
 )
 
 tit_4_tat <- list(
   name = 'tit_4_tat',
-  f = function(prev) {
-    return(prev)
+  f = function(prev, n) {
+    return(prev[n])
   }
 )
 
+pre_tit_4_tat <- list(
+  name = 'pre_tit_4_tat',
+  f = function(prev, n) {
+    return(ifelse(n == 1, 0, prev[n-1]))
+  }
+)
+
+### Game
+
 pd <- list(
   outcome = function(me, you) {
-    return(me - you)
+    return(ifelse(me == you && me == 1, -1, me - you))
   },
   
   round = function(r1, r2) {
     return(c(pd$outcome(r1, r2), pd$outcome(r2, r1)))
   },
   
-  game = function(f1,
-                  f2,
+  game = function(f1, f2,
                   rounds = 10,
                   random = T,
                   start = NA) {
-    names <- list(c(f1$name, f2$name))
-    turns <- array(dim = c(2, rounds + 1), dimnames = names)
-    outcomes <- array(dim = c(2, rounds), dimnames = names)
-    prev <- array(dim = c(2, 1), dimnames = names)
+    names <- c(f1$name, f2$name)
+    turns <- array(dim = c(2, rounds + 1), dimnames = list(names, c('init', 1:10)))
+    outcomes <- array(dim = c(2, rounds), dimnames = list(names, 1:10))
     
     if (!random && !is.na(start) && length(start) == 2) {
-      prev[1:2] <- start
+      turns[1:2, 1] <- start
     } else {
-      prev[1:2] <- sample(c(0, 1), 2, replace = T)
+      turns[1:2, 1] <- sample(c(0, 1), 2, replace = T)
     }
-    
-    turns[1:2, 1] <- prev
+  
     for (i in 1:rounds) {
-      r1 <- f1$f(prev[2])
-      r2 <- f2$f(prev[1])
+      r1 <- f1$f(turns[2,], i)
+      r2 <- f2$f(turns[1,], i)
       turns[1:2, i + 1] <- c(r1, r2)
       outcomes[1:2, i] <- pd$round(r1, r2)
-      prev <- turns[1:2, i + 1]
     }
     game <- list(
         scores = rowSums(outcomes, na.rm = T),
